c12448a02bcda6a56bb1124a06703ce787ac07aa
= <41sukhdevsingh@gmail.com>
2019-11-26 18:17:30 +0530

    Implement query function to work with date
    What changes you have made?
    - Updated query function to accept date and filter the transactions based on given date
    - Added filterRecords to check the query option and filter based on given options
    
    Why this change is necessary?
    - User need to know how many drinks Anna Juice Ltd. sell on a particular day and to whom
    
    What is the impact of the change?
    - App can tell you who drank, how many drinks on any particular day

--- src/query.js
+++ src/query.js
@@ -1,9 +1,35 @@
+const filterRecords = function(records, transactions) {
+  const empId = transactions["--empId"];
+  const date = transactions["--date"];
+
+  if(empId != '' && date != '' && date != undefined){
+  const givenDate = date.split("T")[0];
+    const userRecords = records[empId];
+    const filteredRecord = userRecords.filter(record => {
+      const recordDate = (record["--date"].split('T')[0]);
+      if(recordDate == givenDate){
+        return record;
+      }
+    })
+    return filteredRecord;
+  }
+
+  if(empId != ''){
+    const userRecords = records[empId];
+    return userRecords;
+  }
+
+  const filteredRecord = {error: "Invalid Options"}
+  return filteredRecord;
+};
+
 const query = function(transaction, date, readFile) {
   const filename = "./beverageRecords.json";
   const contents = readFile(filename, "utf8");
   const records = JSON.parse(contents);
-  const userTransactions = records[transaction["--empId"]];
+  const userTransactions = filterRecords(records, transaction);
   if (userTransactions == undefined) return { error: "User doesn't exists" };
+  if(userTransactions.error != undefined) return userTransactions;
   const userData = {
     transactionRecords: userTransactions,
     "--empId": transaction["--empId"]
@@ -11,4 +37,5 @@ const query = function(transaction, date, readFile) {
   return userData;
 };
 
+exports.filterRecords = filterRecords;
 exports.query = query;
--- test/testQuery.js
+++ test/testQuery.js
@@ -1,7 +1,172 @@
 const assert = require("assert");
 const query = require("./../src/query").query;
+const filterRecords = require("./../src/query").filterRecords;
+
+describe("#filterRecords()", () => {
+  it("should filter records if only employee id is given", () => {
+    const records = {
+      "25": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        }
+      ],
+      "26": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-11-26T10:25:09.374Z"
+        }
+      ]
+    };
+    const transaction = {"--empId": "26"};
+    const actual = filterRecords(records, transaction);
+    const expected = [
+      {
+        "--beverage": "orange",
+        "--qty": "1",
+        "--date": "2019-11-26T10:25:06.121Z"
+      },
+      {
+        "--beverage": "orange",
+        "--qty": "5",
+        "--date": "2019-11-26T10:25:09.374Z"
+      }
+    ];
+    assert.deepStrictEqual(actual, expected);
+  });
+  it("should filter records if employee id and a particular date is given", () => {
+    const records = {
+      "25": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        }
+      ],
+      "26": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-11-26T10:25:09.374Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-02-26T10:25:09.374Z"
+        }
+      ]
+    };
+    const transaction = { "--empId": "26", "--date": "2019-11-26"};
+    const actual = filterRecords(records, transaction);
+    const expected = [
+      {
+        "--beverage": "orange",
+        "--qty": "1",
+        "--date": "2019-11-26T10:25:06.121Z"
+      },
+      {
+        "--beverage": "orange",
+        "--qty": "5",
+        "--date": "2019-11-26T10:25:09.374Z"
+      }
+    ];
+    assert.deepStrictEqual(actual, expected);
+  });
+  it("should filter records if a particular date is given", () => {
+    const records = {
+      "25": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        }
+      ],
+      "26": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-11-26T10:25:09.374Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-02-26T10:25:09.374Z"
+        }
+      ]
+    };
+    const transaction = { "--empId": "", "--date": "2019-11-26" };
+    const actual = filterRecords(records, transaction);
+    const expected = { error: "Invalid Options" };
+    assert.deepStrictEqual(actual, expected);
+  });
+  it("should filter records if no options are given", () => {
+    const records = {
+      "25": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        }
+      ],
+      "26": [
+        {
+          "--beverage": "orange",
+          "--qty": "1",
+          "--date": "2019-11-26T10:25:06.121Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-11-26T10:25:09.374Z"
+        },
+        {
+          "--beverage": "orange",
+          "--qty": "5",
+          "--date": "2019-02-26T10:25:09.374Z"
+        }
+      ]
+    };
+    const transaction = { "--empId": "" };
+    const actual = filterRecords(records, transaction);
+    const expected = { error: "Invalid Options" };
+    assert.deepStrictEqual(actual, expected);
+  });
+});
 
 describe("#query()", () => {
+  it("should return error if employee doesn't exists", () => {
+    const transaction = {"--empId": ""};
+    const readFile = function(filename, encoding) {
+      assert.equal(filename, "./beverageRecords.json");
+      assert.equal(encoding, "utf8");
+      const contents =
+        '{"21":[{"--beverage":"Orange","--qty":"5","--date":"2019-11-25T06:16:09.419Z"}]}';
+      return contents;
+    };
+    const date = new Date();
+    const actual = query(transaction, date, readFile);
+    const expected = {
+      error: "Invalid Options"
+    };
+    assert.deepStrictEqual(actual, expected);
+  });
   it("should return error if employee doesn't exists", () => {
     const transaction = {
       "--empId": 11111
